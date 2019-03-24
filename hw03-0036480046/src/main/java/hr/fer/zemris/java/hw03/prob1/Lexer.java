abb3589d5ef214c831645a801b81041722256aeb
Daria2002 <daria.matkovic@fer.hr>
2019-03-24 22:58:04 +0100

    Pass few tests in hw3

--- hw03-0036480046/src/main/java/hr/fer/zemris/java/hw03/prob1/Lexer.java
+++ hw03-0036480046/src/main/java/hr/fer/zemris/java/hw03/prob1/Lexer.java
@@ -21,7 +21,14 @@ public class Lexer {
 	 * @param text text need to be analyzed.
 	 */
 	public Lexer(String text) {
-		data = text.toCharArray();
+		if(text == null) {
+			throw new NullPointerException("Input is null.");
+		}
+		String reducedText = text.replaceAll("\\s+", " ");
+		data = reducedText.trim().toCharArray();
+		for(int i = 0; i < data.length; i++)
+			System.out.println(data[i]);
+		currentIndex = 0;
 	} 
 	
 	/**
@@ -31,31 +38,86 @@ public class Lexer {
 	 */
 	public Token nextToken() {
 		
-		String buildValue = "";
-		for(int i = currentIndex; i < data.length; i++) {
-			// check if string needs to be skipped
-			if('\n' == (data[i]) || '\t' == (data[i]) || 
-					'\r' == (data[i]) || ' ' == (data[i])) {
-				currentIndex = i+1; //TODO: provjeri da ne uzima currentIndex > size
-				break;
-			}
-			if (i > 0) {
-				if(getCharType(data[i]) != getCharType(data[i-1])) {
-					currentIndex = i+1;
-					break;
-				}
-			}
-			buildValue += data[i];
+		// check if token called after EOF
+		// first check if token != null because token.getType() throws exception 
+		// if token is null
+		if(token != null && token.getType().equals(TokenType.EOF)) {
+			throw new LexerException("Called token after EOF");
+		} 
+
+		// check if next token is EOF
+		if(currentIndex > data.length-1) {
+			token = new Token(TokenType.EOF, null);
+			currentIndex++;
+			return token;
+		}
+
+		if(data[currentIndex] == ' ') {
+			currentIndex++;
 		}
 		
-		TokenType type = getTokenType(buildValue);
-		Object value = getObject(type, buildValue);
+		int mode = checkMode(data, currentIndex);
+		// if there is \\ on first place
+		if(data[currentIndex] == '\\') {
+			mode = 2;
+			currentIndex++;
+		}
 		
-		return new Token(type, value);
+		int checkMode = 0;
+		String stringValue = "";
+		stringValue += data[currentIndex];
+		
+		for(int i = currentIndex+1; i <= data.length-1; i++) {
+			checkMode = checkMode(data, i);
+			// if mode changed, etc.before letters, now numbers
+			if(mode == checkMode) {
+				stringValue += data[i];
+			}
+			if(mode != checkMode || i == data.length-1) {
+				currentIndex = i;
+				TokenType type;
+				switch (mode) {
+				case 1:
+					type = TokenType.NUMBER;
+					Long value = Long.parseLong(stringValue);
+					return new Token(type, value);
+				case 2:
+					type = TokenType.WORD;
+					return new Token(type, stringValue);
+				case 3:
+					type = TokenType.SYMBOL;
+					Character valueChar = data[currentIndex];
+					return new Token(type, valueChar);
+				default:
+					type = TokenType.EOF;
+					return new Token(type, null);
+				}
+			}
+		}
+		return new Token(TokenType.EOF, null);
 	}
 	
+	private int checkMode(char[] data, int index) {
+		char c = data[index];
+		// if \\ in the middle
+		if(index-2 >= 0 && data[index-1] == '\\' && data[index-2] != '\\') {
+			return 2;
+		}
+		if(Character.isDigit(c)) {
+			return 1;
+		} else if(Character.isAlphabetic(c)) {
+			return 2;
+		} else if(data[index] == ' ') {
+			return 4;
+		} else if(index <= data.length-1) {
+			// Symbol
+			return 3;
+		}
+		return 0;
+	}
+
 	/**
-	 * Parse to long is value is number, otherwise returns given value
+	 * Parse to long if value is number, otherwise returns given value
 	 * 
 	 * @param type
 	 * @param value
@@ -110,7 +172,7 @@ public class Lexer {
 	 * @return next token
 	 */
 	public Token getToken() {
-		return null;
+		return token;
 	}
 	
 

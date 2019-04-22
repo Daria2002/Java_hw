2687fb9373e1861ab11008b41be279f875598cc9
Daria2002 <daria.matkovic@fer.hr>
2019-04-22 23:53:33 +0200

    Add few very simple things for task4, hw07

--- hw07-0036480046/src/main/java/hr/fer/zemris/java/hw07/demo4/StudentDemo.java
+++ hw07-0036480046/src/main/java/hr/fer/zemris/java/hw07/demo4/StudentDemo.java
@@ -4,17 +4,85 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class StudentDemo {
 	public static void main(String[] args) {
-		Path path = Paths.get("./studenti.txt");
+		Path path = Paths.get(System.getProperty("user.dir") + 
+				"/src/main/java/hr/fer/zemris/java/hw07/demo4/studenti.txt");
+		List<StudentRecord> records = new ArrayList<StudentRecord>();
 		try {
 			List<String> lines = Files.readAllLines(path);
+			records = convert(lines);
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//List<StudentRecord> records = convert(lines);
+		
+		long broj = records.stream()
+				.filter(record -> record.firstExam + record.lastExam + record.lab > 25)
+				.count();
+		
+		System.out.println(broj);
+		
+		long broj5 = records.stream()
+				.filter(record -> record.grade == 5)
+				.count();
+		
+		System.out.println(broj5);
+		
+		List<StudentRecord> odlikasi = records.stream()
+				.filter(record -> record.grade == 5).collect(Collectors.toList());
+		
+		for(StudentRecord odlikas:odlikasi) {
+			System.out.print(odlikas.toString());
+		}
+		
+		List<StudentRecord> odlikasiSortirano = records.stream()
+				.filter(record -> record.grade == 5)
+				.sorted((o1, o2) -> Double.compare(
+						o2.firstExam + o2.lastExam + o2.lab,
+						o1.firstExam + o1.lastExam + o1.lab))
+				.collect(Collectors.toList());
+		
+		for(StudentRecord odlikas:odlikasiSortirano) {
+			System.out.print(odlikas.toString());
+		}
+		
+		List<String> nepolozeniJMBAGovi = records.stream()
+				.filter(record -> record.grade == 1)
+				.sorted((o1, o2) -> Integer.compare(Integer.parseInt(o1.jmbag),
+						Integer.parseInt(o2.jmbag))).map(x -> x.jmbag)
+						.collect(Collectors.toList());
+		
+		for(String odlikas:nepolozeniJMBAGovi) {
+			System.out.println(odlikas);
+		}
+		
+		//Map<Integer, List<StudentRecord>> mapaPoOcjenama = records.stream()
+	}
+
+	private static List<StudentRecord> convert(List<String> lines) {
+		List<StudentRecord> studentList = new ArrayList<StudentRecord>();
+		
+		for(String line:lines) {
+			if(line.isBlank() || line.isEmpty()) {
+				continue;
+			}
+			
+			String[] lineArray = line.split("\\s+");
+			
+			studentList.add(new StudentRecord(lineArray[0], lineArray[1],
+					lineArray[2], Double.parseDouble(lineArray[3]),
+					Double.parseDouble(lineArray[4]), Double.parseDouble(lineArray[5]), 
+					Integer.parseInt(lineArray[6])));
+					
+		}
+		
+		return studentList;
 	}
 }
--- hw07-0036480046/src/main/java/hr/fer/zemris/java/hw07/demo4/StudentRecord.java
+++ hw07-0036480046/src/main/java/hr/fer/zemris/java/hw07/demo4/StudentRecord.java
@@ -1,11 +1,30 @@
 package hr.fer.zemris.java.hw07.demo4;
 
 public class StudentRecord {
+	
 	public String jmbag;
 	public String lastName;
 	public String firstName;
-	public int firstExam;
-	public int lastExam;
-	public int lab;
+	public double firstExam;
+	public double lastExam;
+	public double lab;
 	public int grade;
+	
+	public StudentRecord(String jmbag, String lastName, String firstName, double d, double e, double f,
+			int grade) {
+		this.jmbag = jmbag;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.firstExam = d;
+		this.lastExam = e;
+		this.lab = f;
+		this.grade = grade;
+	}
+	
+	@Override
+	public String toString() {
+		String student = jmbag + "\t" + lastName + "\t" + firstName + "\t" +
+	firstExam + "\t" + lastExam + "\t" + lab + "\t" + grade + "\n";
+		return student.toString();
+	}
 }

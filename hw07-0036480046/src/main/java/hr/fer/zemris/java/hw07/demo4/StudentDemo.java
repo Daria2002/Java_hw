42b4ca5200c2dca988bac2b325aab239736b7125
Daria2002 <daria.matkovic@fer.hr>
2019-04-23 23:30:35 +0200

    First version of whole hw07

--- hw07-0036480046/src/main/java/hr/fer/zemris/java/hw07/demo4/StudentDemo.java
+++ hw07-0036480046/src/main/java/hr/fer/zemris/java/hw07/demo4/StudentDemo.java
@@ -6,6 +6,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -23,49 +26,59 @@ public class StudentDemo {
 			e.printStackTrace();
 		}
 		
-		long broj = records.stream()
-				.filter(record -> record.firstExam + record.lastExam + record.lab > 25)
-				.count();
-		
+		long broj = vratiBodovaViseOd25(records);
+		printFrame(1);
 		System.out.println(broj);
 		
-		long broj5 = records.stream()
-				.filter(record -> record.grade == 5)
-				.count();
-		
+		long broj5 = vratiBrojOdlikasa(records);
+		printFrame(2);
 		System.out.println(broj5);
 		
-		List<StudentRecord> odlikasi = records.stream()
-				.filter(record -> record.grade == 5).collect(Collectors.toList());
-		
+		List<StudentRecord> odlikasi = vratiListuOdlikasa(records);
+		printFrame(3);
 		for(StudentRecord odlikas:odlikasi) {
 			System.out.print(odlikas.toString());
 		}
 		
-		List<StudentRecord> odlikasiSortirano = records.stream()
-				.filter(record -> record.grade == 5)
-				.sorted((o1, o2) -> Double.compare(
-						o2.firstExam + o2.lastExam + o2.lab,
-						o1.firstExam + o1.lastExam + o1.lab))
-				.collect(Collectors.toList());
-		
+		List<StudentRecord> odlikasiSortirano = vratiSortiranuListuOdlikasa(records);
+		printFrame(4);
 		for(StudentRecord odlikas:odlikasiSortirano) {
 			System.out.print(odlikas.toString());
 		}
 		
-		List<String> nepolozeniJMBAGovi = records.stream()
-				.filter(record -> record.grade == 1)
-				.sorted((o1, o2) -> Integer.compare(Integer.parseInt(o1.jmbag),
-						Integer.parseInt(o2.jmbag))).map(x -> x.jmbag)
-						.collect(Collectors.toList());
-		
-		for(String odlikas:nepolozeniJMBAGovi) {
-			System.out.println(odlikas);
+		List<String> nepolozeniJMBAGovi = vratiPopisNepolozenih(records);
+		printFrame(5);
+		for(String jmbag:nepolozeniJMBAGovi) {
+			System.out.println(jmbag);
 		}
 		
-		//Map<Integer, List<StudentRecord>> mapaPoOcjenama = records.stream()
+		Map<Integer, List<StudentRecord>> mapaPoOcjenama = razvrstajStudentePoOcjenama(records);
+		printFrame(6);
+		for(Integer ocjena:mapaPoOcjenama.keySet()) {
+			System.out.println("Ocjena " + ocjena + ":" + mapaPoOcjenama.get(ocjena));
+		}
+		
+		Map<Integer, Integer> mapaPoOcjenama2 = vratiBrojStudenataPoOcjenama(records);
+		printFrame(7);
+		for(Integer ocjena:mapaPoOcjenama2.keySet()) {
+			System.out.println("Ocjena " + ocjena + ":" + mapaPoOcjenama2.get(ocjena));
+		}
+		
+		Map<Boolean, List<StudentRecord>> prolazNeprolaz = razvrstajProlazPad(records);
+		printFrame(8);
+		for(Boolean stanje:prolazNeprolaz.keySet()) {
+			System.out.println((stanje == false ? "neprolaz:" : "prolaz:") + prolazNeprolaz.get(stanje));
+		}
 	}
 
+	private static void printFrame(int taskNumber) {
+		if(taskNumber != 1) {
+			System.out.println();
+		}
+		System.out.println("Zadatak " + taskNumber);
+		System.out.println("=========");
+	}
+	
 	private static List<StudentRecord> convert(List<String> lines) {
 		List<StudentRecord> studentList = new ArrayList<StudentRecord>();
 		
@@ -84,5 +97,53 @@ public class StudentDemo {
 		}
 		
 		return studentList;
+	}
+	
+	private static long vratiBodovaViseOd25(List<StudentRecord> records) {
+		return records.stream()
+				.filter(record -> record.firstExam + record.lastExam + record.lab > 25)
+				.count();
+	}
+	
+	private static long vratiBrojOdlikasa(List<StudentRecord> records) {
+		return records.stream()
+				.filter(record -> record.grade == 5)
+				.count();
+	}
+	
+	private static List<StudentRecord> vratiListuOdlikasa(List<StudentRecord> records) {
+		return records.stream()
+				.filter(record -> record.grade == 5).collect(Collectors.toList());
+	}
+	
+	private static List<StudentRecord> vratiSortiranuListuOdlikasa(List<StudentRecord> records) {
+		return records.stream()
+				.filter(record -> record.grade == 5)
+				.sorted((o1, o2) -> Double.compare(
+						o2.firstExam + o2.lastExam + o2.lab,
+						o1.firstExam + o1.lastExam + o1.lab))
+				.collect(Collectors.toList());
+	}
+	
+	private static List<String> vratiPopisNepolozenih(List<StudentRecord> records) {
+		return records.stream()
+				.filter(record -> record.grade == 1)
+				.sorted((o1, o2) -> Integer.compare(Integer.parseInt(o1.jmbag),
+						Integer.parseInt(o2.jmbag))).map(x -> x.jmbag)
+						.collect(Collectors.toList());
+	}
+	
+	private static Map<Integer, List<StudentRecord>> razvrstajStudentePoOcjenama(List<StudentRecord> records) {
+		return records.stream().collect(Collectors.groupingBy(student -> student.grade));
+	}
+
+	private static Map<Integer, Integer> vratiBrojStudenataPoOcjenama(List<StudentRecord> records) {
+		return records.stream().collect(Collectors.toMap(student -> student.grade, 
+		        s -> 1, 
+		        (left, right) -> left + right));
+	}
+	
+	private static Map<Boolean, List<StudentRecord>> razvrstajProlazPad(List<StudentRecord> records) {
+		return records.stream().collect(Collectors.partitioningBy(student -> student.grade > 1));
 	}
 }

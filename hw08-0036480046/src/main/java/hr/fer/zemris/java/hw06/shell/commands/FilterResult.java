28cd3ca7b18d25d598eff97edbfde695b9062d32
Daria2002 <daria.matkovic@fer.hr>
2019-04-28 00:17:45 +0200

    Add something dummy

---
+++ hw08-0036480046/src/main/java/hr/fer/zemris/java/hw06/shell/commands/FilterResult.java
@@ -0,0 +1,16 @@
+package hr.fer.zemris.java.hw06.shell.commands;
+
+public class FilterResult {
+
+	public String toString() {
+		
+	}
+	
+	public int numberOfGroups() {
+		
+	}
+	
+	public String group(int index) {
+		
+	}
+}

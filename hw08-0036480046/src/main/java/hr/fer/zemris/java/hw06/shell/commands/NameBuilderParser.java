b355c03651e9a9fd6d9978d380760d8327888485
Daria2002 <daria.matkovic@fer.hr>
2019-04-28 15:44:53 +0200

    Add some simple things in hw08

--- hw08-0036480046/src/main/java/hr/fer/zemris/java/hw06/shell/commands/NameBuilderParser.java
+++ hw08-0036480046/src/main/java/hr/fer/zemris/java/hw06/shell/commands/NameBuilderParser.java
@@ -1,5 +1,8 @@
 package hr.fer.zemris.java.hw06.shell.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Class that represents parser for building name for new dir
  * @author Daria Matković
@@ -7,6 +10,25 @@ package hr.fer.zemris.java.hw06.shell.commands;
  */
 public class NameBuilderParser {
 
+	/**
+	 * Method that build new object of NameBuilde instead of making new class
+	 * that builds NameBuilder
+	 * @param t
+	 * @return
+	 */
+	static NameBuilder text(String t) { 
+		return (result, sb) -> {sb.append(t);}; 
+	}
+	
+	static NameBuilder group(int index) {
+		return (result, sb) -> {sb.append(result.group(index));}; 
+	}
+	
+	static NameBuilder group(int index, char padding, int minWidth) {
+		return (result, sb) -> {sb.append(String.format("%" + minWidth + "s", 
+				result.group(index).replace(' ', padding)));};
+	}
+	
 	private String expression;
 	
 	/**
@@ -17,18 +39,36 @@ public class NameBuilderParser {
 		this.expression = izraz;
 	}
 	
+	private static class NameBuilderComposite implements NameBuilder {
+
+		public List<NameBuilder> nameBuilderList = new ArrayList<NameBuilder>();
+		
+		public NameBuilderComposite(List<NameBuilder> nameBuilderList) {
+			this.nameBuilderList = nameBuilderList;
+		}
+		
+		@Override
+		public void execute(FilterResult result, StringBuilder sb) {
+			for(NameBuilder nameBuilder : nameBuilderList) {
+				nameBuilder.execute(result, sb);
+			}
+		}
+	}
+	
 	/**
 	 * Gets name builder
 	 * @return name builder
 	 */
 	public NameBuilder getNameBuilder() {
-		return new NameBuilder() {
-			
-			@Override
-			public void execute(FilterResult result, StringBuilder sb) {
-				// TODO Auto-generated method stub
-				
-			}
-		};
+		List<NameBuilder> nameBuilderList = makeNameBuilders(expression);
+		
+		return new NameBuilderComposite(nameBuilderList);
+	}
+
+	private List<NameBuilder> makeNameBuilders(String expression) {
+		
+		
+		
+		return null;
 	}
 }

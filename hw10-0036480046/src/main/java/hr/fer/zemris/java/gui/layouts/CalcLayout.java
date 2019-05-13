227fd49bb54e0958947e03d5c8cfd9dbf84ecde6
Daria2002 <daria.matkovic@fer.hr>
2019-05-13 14:18:26 +0200

    Some things look almost like they shoul look.

--- hw10-0036480046/src/main/java/hr/fer/zemris/java/gui/layouts/CalcLayout.java
+++ hw10-0036480046/src/main/java/hr/fer/zemris/java/gui/layouts/CalcLayout.java
@@ -3,20 +3,27 @@ package hr.fer.zemris.java.gui.layouts;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Insets;
 import java.awt.LayoutManager2;
 import java.awt.Rectangle;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.swing.JLabel;
 
 public class CalcLayout implements LayoutManager2 {
 
 	private int spaceBetweenRowsAndColumns;
 	private Map<Component, RCPosition> components;
-	private static final int HEIGHT = 5;
-	private static final int WIDTH = 7;
+	private static final int NUMBER_OF_ROWS = 5;
+	private static final int NUMBER_OF_COMLUMNS = 7;
+	private static final int TOTAL_NUMBER_OF_COMPONENTS = 31;
+	private int[] columnsWidth;
+	private int[] rowsHeight;
 	
 	public CalcLayout() {
 		this(0);
@@ -25,6 +32,8 @@ public class CalcLayout implements LayoutManager2 {
 	public CalcLayout(int space) {
 		spaceBetweenRowsAndColumns = space;
 		components = new HashMap<Component, RCPosition>();
+		columnsWidth = new int[7];
+		rowsHeight = new int[5];
 	}
 	
 	@Override
@@ -41,17 +50,104 @@ public class CalcLayout implements LayoutManager2 {
 
 	@Override
 	public Dimension preferredLayoutSize(Container parent) {
-		return new Dimension(500, 500);
+		if(parent.getWidth() != 0 && parent.getHeight() != 0) {
+			columnsWidth = calculateComponentSize(parent.getWidth(), NUMBER_OF_COMLUMNS);
+			rowsHeight = calculateComponentSize(parent.getHeight(), NUMBER_OF_ROWS);
+			return new Dimension(parent.getWidth(), parent.getHeight());
+		}
+		
+		Dimension maxDim = getMaxDimensions();
+		Arrays.fill(columnsWidth, maxDim.width);
+		Arrays.fill(rowsHeight, maxDim.height);
+		
+		return new Dimension(maxDim.width * NUMBER_OF_COMLUMNS + 
+				NUMBER_OF_COMLUMNS * spaceBetweenRowsAndColumns, 
+				maxDim.height * NUMBER_OF_ROWS + NUMBER_OF_ROWS * spaceBetweenRowsAndColumns);
+	}
+	
+	private int[] calculateComponentSize(int size, int numberOfComponents) {
+		int[] componentsSize = new int[TOTAL_NUMBER_OF_COMPONENTS]; 
+		
+		if(size % numberOfComponents != 0) {
+			// number of elements with smaller size
+			int smallerSize = size % numberOfComponents;
+			
+			for(int i = 0; i < numberOfComponents; i++) {
+				if(i % 2 == 0 && smallerSize > 0) {
+					componentsSize[i] = size/numberOfComponents - 1;
+					smallerSize--;
+				} else {
+					componentsSize[i] = size/numberOfComponents;
+				}
+			}
+		}
+		
+		Arrays.fill(componentsSize, size/numberOfComponents);
+		return componentsSize;
+	}
+
+	private Dimension getMaxDimensions() {
+		int maxWidth = 0; 
+		int maxHeight = 0;
+		
+		for(Component comp : components.keySet()) {
+			if(comp.getSize().height == 0) {
+				// calculate size so largest element can show all informations
+				return calculateSize();
+			}
+			
+			if(comp.getWidth() > maxWidth) {
+				maxWidth = comp.getWidth();
+				
+			} if(comp.getHeight() > maxHeight) {
+				maxHeight = comp.getHeight();
+			}
+		}
+		
+		return new Dimension(maxWidth, maxHeight);
+	}
+
+	private Dimension calculateSize() {
+		Font labelFont;
+		String labelText;
+		int stringWidth;
+		int stringHeight;
+		int maxWidth = 0; 
+		int maxHeight = 0;
+		
+		for(Component comp : components.keySet()) {
+			labelFont = comp.getFont();
+			JLabel compLabel = (JLabel) comp;
+			labelText = compLabel.getText();
+			
+			stringWidth = comp.getFontMetrics(labelFont).stringWidth(labelText);
+			stringHeight = comp.getFontMetrics(labelFont).getHeight();
+			
+			if(stringHeight > maxHeight) {
+				maxHeight = stringHeight;
+			} if(stringWidth > maxWidth) {
+				maxWidth = stringWidth;
+			}
+		}
+		
+		return new Dimension(maxWidth, maxHeight);
 	}
 
 	@Override
 	public Dimension minimumLayoutSize(Container parent) {
-		return new Dimension(500, 500);
+		if(parent.getWidth() != 0 && parent.getHeight() != 0) {
+			return new Dimension(parent.getWidth(), parent.getHeight());
+		}
+		
+		Dimension maxDim = getMaxDimensions();
+		
+		return new Dimension(maxDim.width * NUMBER_OF_COMLUMNS, 
+				maxDim.height * NUMBER_OF_ROWS);
 	}
 
 	@Override
 	public Dimension maximumLayoutSize(Container target) {
-		return new Dimension(500, 500);
+		return preferredLayoutSize(target);
 	}
 
 	@Override
@@ -76,44 +172,20 @@ public class CalcLayout implements LayoutManager2 {
 							position.getColumn() * this.spaceBetweenRowsAndColumns;
 					y = (position.getRow()-1) * parent.getHeight()/numberOfComponents +
 							position.getRow() * this.spaceBetweenRowsAndColumns;
-					w = 100 * 4;
+					w = columnsWidth[0] * 4 + 3*spaceBetweenRowsAndColumns;
 					
 				} else {
 					x = (position.getColumn()-1) * parent.getWidth()/numberOfComponents;
 					y = (position.getRow()-1) * parent.getHeight()/numberOfComponents;
-					w = 100;
+					w = columnsWidth[position.getColumn()-1];
 				}
 				
 				
-				int h = 100;
+				int h = rowsHeight[position.getRow()-1];
 				comp.setBounds(x, y, w, h);
 			}
 			i++;
 		}
-	}
-
-	private int getMaximumWidth() {
-		int maxWidth = -1;
-		
-		for(Component comp : components.keySet()) {
-			if(comp.getX() > maxWidth) {
-				maxWidth = comp.getX();
-			}
-		}
-		
-		return maxWidth;
-	}
-	
-	private int getMaximumHeight() {
-		int maxHeight = -1;
-		
-		for(Component comp : components.keySet()) {
-			if(comp.getX() > maxHeight) {
-				maxHeight = comp.getY();
-			}
-		}
-		
-		return maxHeight;
 	}
 	
 	@Override

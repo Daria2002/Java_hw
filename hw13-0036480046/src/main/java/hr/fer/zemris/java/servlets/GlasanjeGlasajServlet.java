ae80f9c06e1e946d5fd6e7cdc190d1c32951aa54
Daria2002 <daria.matkovic@fer.hr>
2019-06-04 00:15:21 +0200

    Add some impl for task7

--- hw13-0036480046/src/main/java/hr/fer/zemris/java/servlets/GlasanjeGlasajServlet.java
+++ hw13-0036480046/src/main/java/hr/fer/zemris/java/servlets/GlasanjeGlasajServlet.java
@@ -1,7 +1,17 @@
 package hr.fer.zemris.java.servlets;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,8 +25,38 @@ public class GlasanjeGlasajServlet extends HttpServlet {
 		// Zabiljezi glas...
 		String fileName = req.getServletContext().getRealPath("/WEB-INF/glasanje-rezultati.txt");
 		// Napravi datoteku ako je potrebno; ažuriraj podatke koji su u njoj...
-		// ...
-		// ...
+		
+		BufferedReader abc = new BufferedReader(new FileReader(fileName));
+		List<String> lines = new ArrayList<String>();
+
+		String line = abc.readLine();
+		while(line != null) {
+		    lines.add(line);
+		    line = abc.readLine();
+		}
+		abc.close();
+
+		// If you want to convert to a String[]
+		String[] data = lines.toArray(new String[]{});
+		
+		Map<String, String> map = new HashMap<String, String>();
+		
+		String[] names = new String[data.length];
+		String[] ids = new String[data.length];
+		
+		for(int i = 0; i < data.length; i++) {
+			String[] lineData = data[i].split("\t");
+			
+			map.put(lineData[0], lineData[1]);
+		}
+		
+		String key = "1";
+		if(map.containsKey(key)) {
+			map.put(key, map.get(key) + 1);
+		}
+		
+		req.setAttribute("map", map);
+		
 		// Kad je gotovo, pošalji redirect pregledniku I dalje NE generiraj odgovor
 		resp.sendRedirect(req.getContextPath() + "/glasanje-rezultati");
 	}
--- hw13-0036480046/src/main/webapp/WEB-INF/pages/glasanjeIndex.jsp
+++ hw13-0036480046/src/main/webapp/WEB-INF/pages/glasanjeIndex.jsp
@@ -1,8 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
+
+<%
+	String myColor = (String)session.getAttribute("pickedBgCol");	
+
+	if (myColor == null){
+		myColor = "white";
+	}
+%>
+    
 <!DOCTYPE html>
 <html>
-<body>
+<body bgcolor="<%=myColor%>">
 <h1>Glasanje za omiljeni bend:</h1>
 <p>Od sljedećih bendova, koji Vam je bend najdraži? Kliknite na link kako biste
 glasali!</p>

38dc39716b6e0e312c42f28701ee8d00e5957575
Daria2002 <daria.matkovic@fer.hr>
2019-06-11 17:09:12 +0200

    Add some maps

--- hw14-0036480046/src/main/java/hr/fer/zemris/java/servlets/GlasanjeGlasajServlet.java
+++ hw14-0036480046/src/main/java/hr/fer/zemris/java/servlets/GlasanjeGlasajServlet.java
@@ -1,11 +1,6 @@
 package hr.fer.zemris.java.servlets;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import hr.fer.zemris.java.p12.dao.sql.SQLDAO;
 import hr.fer.zemris.java.p12.model.Unos;
 
@@ -32,41 +26,6 @@ public class GlasanjeGlasajServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		// Zabiljezi glas...
-//		
-//		String fileName = req.getServletContext().getRealPath("/WEB-INF/glasanje-rezultati.txt");
-//		// Napravi datoteku ako je potrebno; ažuriraj podatke koji su u njoj...
-//		
-//		BufferedReader abc = new BufferedReader(new FileReader(fileName));
-//		List<String> lines = new ArrayList<String>();
-//
-//		String line = abc.readLine();
-//		while(line != null) {
-//		    lines.add(line);
-//		    line = abc.readLine();
-//		}
-//		abc.close();
-//
-//		// If you want to convert to a String[]
-//		String[] data = lines.toArray(new String[]{});
-//		
-//		// map where ids are keys and values are bend points
-//		Map<String, String> map = new HashMap<String, String>();
-//		
-//		for(int i = 0; i < data.length; i++) {
-//			String[] lineData = data[i].split("\t");
-//			map.put(lineData[0], lineData[1]);
-//		}
-//		
-//		String key = req.getParameter("id");
-//		String newValue = map.get(key) != null ? String.valueOf(Integer.valueOf(map.get(key))+1)
-//				: String.valueOf(1);
-//		map.put(key, newValue);
-//		
-//		req.getSession().setAttribute("map", map);
-		
-		// povecaj broj glasova i setaj atribut map
-		
 		SQLDAO sqlDao = new SQLDAO();
 		
 		long pollId = (long) req.getAttribute("pollId");
@@ -74,7 +33,8 @@ public class GlasanjeGlasajServlet extends HttpServlet {
 		
 		sqlDao.increaseVotes(id);
 		
-		List<Unos> entries = new ArrayList<Unos>();
+		List<Unos> entries = sqlDao.getOptions(pollId);
+		// map with key:id, value:votes
 		Map<Long, Long> map = new HashMap<Long, Long>();
 		
 		for(int i = 0; i < entries.size(); i++) {
@@ -82,44 +42,15 @@ public class GlasanjeGlasajServlet extends HttpServlet {
 		}
 		
 		req.getSession().setAttribute("map", map);
-		
-		
-		/// nova dz
-			
-		StringBuilder fileContent = new StringBuilder();
-		for(String keyEl:map.keySet()) {
-			fileContent.append(keyEl + "\t" + map.get(keyEl) + "\n");
-		}
-		
-		File destFile = new File(fileName);
-		FileOutputStream fileStream = new FileOutputStream(destFile, false); // true to append
-		                   
-		fileStream.write(fileContent.toString().getBytes());
-		fileStream.close();
-		
-		String fileName2 = req.getServletContext()
-				.getRealPath("/WEB-INF/glasanje-definicija.txt");
-		
-		BufferedReader abc2 = new BufferedReader(new FileReader(fileName2));
-		List<String> lines2 = new ArrayList<String>();
 
-		String line2 = abc2.readLine();
-		while(line2 != null) {
-		    lines2.add(line2);
-		    line2 = abc2.readLine();
-		}
-		abc2.close();
-
-		// If you want to convert to a String[]
-		String[] data2 = lines2.toArray(new String[]{});
+		// map with key:id, value:names
+		Map<Long, String> mapIdAndNames = new HashMap<Long, String>();
+		// map with key:id, value:links
+		Map<Long, String> mapIdAndLinks = new HashMap<Long, String>();
 		
-		Map<String, String> mapIdAndNames = new HashMap<String, String>();
-		Map<String, String> mapIdAndLinks = new HashMap<String, String>();
-		
-		for(int i = 0; i < data2.length; i++) {
-			String[] lineData = data2[i].split("\t");
-			mapIdAndNames.put(lineData[0], lineData[1]);
-			mapIdAndLinks.put(lineData[0], lineData[2]);
+		for(int i = 0; i < entries.size(); i++) {
+			mapIdAndNames.put(entries.get(i).getId(), entries.get(i).getTitle());
+			mapIdAndLinks.put(entries.get(i).getId(), entries.get(i).getDesc());
 		}
 		
 		req.getSession().setAttribute("mapIdAndNames", mapIdAndNames);

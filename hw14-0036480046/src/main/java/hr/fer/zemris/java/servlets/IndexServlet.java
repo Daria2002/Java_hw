c44115a25149e10f31a74cf1078d0341c747ca8b
Daria2002 <daria.matkovic@fer.hr>
2019-06-11 18:15:53 +0200

    Add impl in IndexServlet, this is impl before adding jsp files and servlets from hw13

--- hw14-0036480046/src/main/java/hr/fer/zemris/java/servlets/IndexServlet.java
+++ hw14-0036480046/src/main/java/hr/fer/zemris/java/servlets/IndexServlet.java
@@ -1,15 +1,10 @@
 package hr.fer.zemris.java.servlets;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import hr.fer.zemris.java.p12.dao.sql.SQLDAO;
-import hr.fer.zemris.java.p12.model.Poll;
 
 public class IndexServlet extends HttpServlet {
 
@@ -21,17 +16,6 @@ public class IndexServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		SQLDAO sqlDao = new SQLDAO();
-		List<Poll> polls = sqlDao.getDefinedPolls();
-		
-		Map<Long, String> pollsMap = new HashMap<Long, String>();
-		
-		for(Poll poll:polls) {
-			pollsMap.put(poll.getPollId(), poll.getTitle());
-		}
-		
-		req.getSession().setAttribute("polls", pollsMap);
-		
-	    req.getRequestDispatcher("/choosePoll.jsp").forward(req, resp);
+	    resp.sendRedirect("/servleti/index.html");
 	}
 }

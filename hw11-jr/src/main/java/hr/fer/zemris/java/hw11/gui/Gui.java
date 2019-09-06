68feae8c588756ffeac74299b5351a2273ae0a08
Daria2002 <daria.matkovic@fer.hr>
2019-09-06 15:30:37 +0200

    Add some features in gui used for jr preparation

---
+++ hw11-jr/src/main/java/hr/fer/zemris/java/hw11/gui/Gui.java
@@ -0,0 +1,296 @@
+package hr.fer.zemris.java.hw11.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class Gui extends JFrame {
+	
+	List<Person> people = new ArrayList<Person>();
+	
+	public Gui() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocation(500, 500);
+		setSize(700, 500);
+		setTitle("Gui jr");
+		
+		people = getPeopleFromFile();
+		
+		for(int i = 0; i < people.size(); i++) {
+			System.out.println(people.get(i).getName() +  " " + people.get(i).getAge());
+		}
+		
+		initGui();
+	}
+	
+	private List<Person> getPeopleFromFile() {
+		String fileNameAndDesktop = "/Desktop/testGUI";
+		File desktopFile = new File(System.getProperty("user.home"), fileNameAndDesktop);
+		
+		List<String> fileText = null;
+		
+		try {
+			fileText = Files.readAllLines(desktopFile.toPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		List<Person> tempPeople = new ArrayList<Person>();
+		
+		// skip "ime godine", so i = 1
+		for(int i = 0; i < fileText.size(); i++) {
+			String[] lineInfo = fileText.get(i).trim().split("\\s+");
+			
+			tempPeople.add(new Person(lineInfo[0], Integer.valueOf(lineInfo[1])));
+		}
+		
+		return tempPeople;
+	}
+
+	private void addNewPersonToFile(Person newPerson) {
+		String fileNameAndDesktop = "/Desktop/testGUI";
+		File desktopFile = new File(System.getProperty("user.home"), fileNameAndDesktop);
+		
+		String newLine = newPerson.getName() + " " + newPerson.getAge() + "\n";
+		
+		try {
+			Files.write(Paths.get(desktopFile.getPath()), newLine.getBytes(),
+					StandardOpenOption.APPEND);
+			
+		} catch (IOException e) {
+			System.out.println("Can't add new person to file");
+			e.printStackTrace();
+		}
+	}
+	
+	private void removeLineInFile(String name) {
+		String fileNameAndDesktop = "/Desktop/testGUI";
+		File desktopFile = new File(System.getProperty("user.home"), fileNameAndDesktop);
+		
+		List<String> fileText = null;
+		
+		try {
+			fileText = Files.readAllLines(desktopFile.toPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		List<Person> tempPeople = new ArrayList<Person>();
+		
+		for(int i = 0; i < fileText.size(); i++) {
+			String[] lineInfo = fileText.get(i).trim().split("\\s+");
+			
+			if(lineInfo[0].equals(name)) {
+				continue;
+			}
+			
+			tempPeople.add(new Person(lineInfo[0], Integer.valueOf(lineInfo[1])));
+		}
+		
+		String peopleString = "";
+		
+		for(int i = 0; i < tempPeople.size(); i++) {
+			peopleString += tempPeople.get(i).toString();
+		}
+		
+		
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(desktopFile));
+			bw.write(peopleString);
+			bw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private final Action addUser = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			JFrame frame = new JFrame("Enter user info");
+			frame.setSize(300, 150);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			JPanel panel = new JPanel();
+			frame.add(panel);
+
+			JLabel userLabel = new JLabel("Name");
+			userLabel.setBounds(10, 10, 80, 25);
+			panel.add(userLabel);
+			
+			JTextField userText = new JTextField(20);
+			userText.setBounds(100, 10, 160, 25);
+			panel.add(userText);
+			
+			JLabel ageLabel = new JLabel("Age");
+			userLabel.setBounds(10, 20, 80, 50);
+			panel.add(ageLabel);
+			
+			JTextField ageText = new JTextField(3);
+			ageText.setBounds(100, 20, 160, 50);
+			panel.add(ageText);
+
+			JButton save = new JButton("save user");
+			
+			save.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					String name = userText.getText();
+					Integer age = Integer.valueOf(ageText.getText());
+					
+					Person newPerson = new Person(name, age);
+					people.add(newPerson);
+					addNewPersonToFile(newPerson);
+					// close window :)
+					frame.dispose();
+				}
+
+			});
+			
+			save.setBounds(10, 80, 80, 25);
+			panel.add(save);
+			
+			JButton cancel = new JButton("cancel");
+			cancel.setBounds(180, 80, 80, 25);
+			
+			cancel.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					frame.dispose();
+				}
+			});
+			
+			panel.add(cancel);
+			
+			frame.setVisible(true);
+		}
+	};
+	
+	private final Action removeUser = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JFrame frame = new JFrame("Enter name of user to remove");
+			frame.setSize(300, 150);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			JPanel panel = new JPanel();
+			frame.add(panel);
+			
+			JLabel nameLabel = new JLabel("Name");
+			nameLabel.setBounds(10, 10, 80, 25);
+			panel.add(nameLabel);
+			
+			JTextField nameText = new JTextField(20);
+			nameText.setBounds(100, 10, 160, 25);
+			panel.add(nameText);
+			
+			JButton remove = new JButton("remove user");
+			
+			remove.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+
+					for(int i = 0; i < people.size(); i++) {
+						if(people.get(i).getName().equals(nameText.getText())) {
+							people.remove(i);
+							frame.dispose();
+						}
+					}
+					removeLineInFile(nameText.getText());
+				}
+
+			});
+			
+			remove.setBounds(10, 80, 80, 25);
+			panel.add(remove);
+			
+			JButton cancel = new JButton("cancel");
+			cancel.setBounds(180, 80, 80, 25);
+			
+			cancel.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					frame.dispose();
+				}
+			});
+			
+			panel.add(cancel);
+			
+			frame.setVisible(true);
+			
+		}
+	};
+
+	private final Action listNames = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	};
+	
+	private void initGui() {
+		Container cp = getContentPane();
+		cp.setLayout(new BorderLayout());
+		
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menuUser = new JMenu("User");
+		
+		JMenuItem addUserItem = new JMenuItem(addUser);
+		// set text sets name of menu item :)
+		addUserItem.setText("Add user");
+		menuUser.add(addUserItem);
+		
+		JMenuItem removeUserItem = new JMenuItem(removeUser);
+		removeUserItem.setText("Remove user");
+		menuUser.add(removeUserItem);
+		
+//		menuUser.add(openUser);
+//		menuUser.add(openAllUsers);
+		menuBar.add(menuUser);
+		
+		JMenu menuInfo = new JMenu("Info");
+		JMenuItem listNamesItem = new JMenuItem(listNames);
+		listNamesItem.setText("List names");
+		menuUser.add(listNamesItem);
+		
+		menuBar.add(menuInfo);
+		
+		setJMenuBar(menuBar);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {new Gui().setVisible(true);});
+	}
+	
+}
---
+++ hw11-jr/src/main/java/hr/fer/zemris/java/hw11/gui/Person.java
@@ -0,0 +1,34 @@
+package hr.fer.zemris.java.hw11.gui;
+
+public class Person {
+
+	private String name;
+	private Integer age;
+	
+	public Person(String name, Integer age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return name + " " + age.toString() + "\n";
+	}
+	
+}
